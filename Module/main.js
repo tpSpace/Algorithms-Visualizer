993a103dc4462b390d1a13b1679e4ad855901f0a
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-02-03 12:54:32 +0700

    Draw matrix & standardize const

--- Module/main.js
+++ Module/main.js
@@ -1,7 +1,13 @@
 var v = document.getElementById('myCanvas');
 var ctx = v.getContext('2d');
 
-const width = 800;
-const height = 600;
-
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+const SQUARE_SIZE = 40;
+// Draw matrix
+for (var i = 0; i < CANVAS_WIDTH; i += SQUARE_SIZE) {
+    for (var j = 0; j < CANVAS_HEIGHT; j += SQUARE_SIZE) {
+        ctx.strokeRect(i, j, SQUARE_SIZE, SQUARE_SIZE);
+    }
+}
 
